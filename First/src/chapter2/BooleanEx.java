85a523e04bc1728d0caaa72c4e395cc4ca34d34e
Sojin <imadeit777@naver.com>
2024-07-17 17:14:38 +0900

    first commit for class

---
+++ First/src/chapter2/BooleanEx.java
@@ -0,0 +1,16 @@
+package chapter2;
+
+public class BooleanEx {
+
+	public static void main(String[] args) {
+		
+		boolean isMarried = true;
+		System.out.println(isMarried);
+		
+		int a = 10;
+		double b = 2.0;
+
+		System.out.println((int)a+b);
+	}
+
+}
